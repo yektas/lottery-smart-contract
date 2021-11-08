ce639821b3429c21626d4060a08ba365e273019d
yektas <ahmetsercanyektas@gmail.com>
2021-11-08 12:38:51 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Lottery Contract
+
+
+
+## Variables
+
+| Name    | Purpose                                       |
+| ------- | --------------------------------------------- |
+| manager | Address of person who created the contract    |
+| players | Array of addresses of people who have entered |
+
+## Functions
+
+| Name       | Purpose                                               |
+| ---------- | ----------------------------------------------------- |
+| enter      | Enters a player into the lottery                      |
+| pickWinner | Randomly picks a winner and sends them the prize pool |
---
+++ lottery.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Lottery {
+    address public manager;
+    address[] public players;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > .01 ether);
+        players.push(msg.sender);
+    }
+
+    function pickWinner() public payable {}
+
+    function random() private view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(block.difficulty, block.timestamp, players)
+                )
+            );
+    }
+}
